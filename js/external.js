fc5051c54d13721f01fbf4c08ab075e09aa2d351
Louie Espinosa <louie.espinosa@proton.me>
2022-06-02 16:57:29 -0500

    Completed functions exercise, and small change to external.js

--- js/external.js
+++ js/external.js
@@ -9,18 +9,17 @@ var mer = prompt("please rate The little mermaid from 1 to 5")*wastedMoney;
 var bB = prompt("please rate Brother Bear from 1 to 5")*wastedMoney;
 var herc = prompt("please rate Hercules from 1 to 5")*wastedMoney;
 var spent = mer + bB + herc;
-alert("Youve wasted "+ spent + " dollars!");
+alert("You've wasted "+ spent + " dollars!");
 
 var wages = 380;
 var google = prompt("How many hours did you grind at Google this week?")*(wages + 20);
 var amazon = prompt("How many hours did you grind at Amazon this week?")*wages;
 var faceBook = prompt("How many hours did you grind at Facebook this week?")*(wages - 30);
 var weeklyWage = google + amazon + faceBook;
-alert("Youve earned " +weeklyWage);
+alert("You've earned " +weeklyWage);
 
 var enrolled = confirm("Is the class still available?") && confirm("Does the class schedule create a conflict?");
 alert("You are enrolled!" + enrolled);
 
 var offerStillStands = confirm("Are you buying more than two items?") && confirm("Has the offer expired?") || confirm("Are you a Premium member?");
 alert("Enjoy your bonus! " + offerStillStands);
-
