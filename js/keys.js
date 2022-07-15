f2e4fb661aa43086284340353a0fbfcfea8cc945
Louie Espinosa <louie.espinosa@proton.me>
2022-07-15 13:46:25 -0500

    partially completed mapbox_maps_api exercise

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,4 @@
+// do not enclose in braces ,or IFFE because we need access to the data
+    "use strict";
 //my api keys go here but dont push them to github (we'll use gitignore)
-const MAPBOX_API_KEY = "pk.eyJ1IjoibGFlc3Bpbm9zYSIsImEiOiJjbDVsNjllZ2gwZ3Q3M2ptbWEzOGoxZGdwIn0.o8uhoKai8kWFQ4D0Vae0Vw";
+    const MAPBOX_API_KEY = "pk.eyJ1IjoibGFlc3Bpbm9zYSIsImEiOiJjbDVsNjllZ2gwZ3Q3M2ptbWEzOGoxZGdwIn0.o8uhoKai8kWFQ4D0Vae0Vw";
