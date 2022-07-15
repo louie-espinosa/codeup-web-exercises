b89b140e523dc9118f80758581b554b3f96ab454
Louie Espinosa <louie.espinosa@proton.me>
2022-07-15 16:56:02 -0500

    Made small changes to mapbox; Created and tested weather_map project

--- js/keys.js
+++ js/keys.js
@@ -2,3 +2,4 @@
     "use strict";
 //my api keys go here but dont push them to github (we'll use gitignore)
     const MAPBOX_API_KEY = "pk.eyJ1IjoibGFlc3Bpbm9zYSIsImEiOiJjbDVsNjllZ2gwZ3Q3M2ptbWEzOGoxZGdwIn0.o8uhoKai8kWFQ4D0Vae0Vw";
+const OPEN_WEATHER_API_KEY = "659a8f23362cbcad0d1ac593844b9b4e";
